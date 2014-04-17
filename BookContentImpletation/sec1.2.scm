57dedbd894105451d8206086256e18ac530c5270
Chenlin Wang <chenlin.wang520@gmail.com>
2014-04-17 21:09:32 +0800

    Finish book sec1.2.2

--- BookContentImpletation/sec1.2.scm
+++ BookContentImpletation/sec1.2.scm
@@ -15,3 +15,36 @@
         (* result n)
         (up (* result next) (+ next 1))))
   (up 1 1))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Fibonacci
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (Fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else
+         (+ (Fib (- n 1)) (Fib (- n 2))))))
+
+(define (Fib-I n)
+  (define (Fib-iter former latter n)
+    (if (= n 0)
+        former
+        (Fib-iter latter (+ former latter) (- n 1))))
+  (Fib-iter 0 1 n))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Count the change
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (Count money kind)
+  (define (money-table kind)
+    (cond ((= kind 0) 0)
+          ((= kind 1) 1)
+          ((= kind 2) 5)
+          ((= kind 3) 10)
+          ((= kind 4) 20)
+          ((= kind 5) 50)
+          ((= kind 6) 100)
+          (else 0)))
+  (cond ((or (< money 0) (= kind 0)) 0)
+        ((= money 0) 1)
+        (else (+ (Count (- money (money-table kind)) kind) (Count money (- kind 1))))))
