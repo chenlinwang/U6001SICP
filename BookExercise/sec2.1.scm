8bc1cd1a25c631ede3f5805472111a640fad343e
Chenlin Wang <chenlin.wang520@gmail.com>
2014-04-28 11:34:20 +0800

    Comment the exercise 1.28

--- BookExercise/sec2.1.scm
+++ BookExercise/sec2.1.scm
@@ -315,3 +315,181 @@
 ;The plus is:
 (define (church-add x y)
   (lambda (f) ((x f) ((y f) x))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 2.7
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Alyssa's program is incomplete because she has not specified the implementation of the interval abstraction. Here is a definition of the interval constructor:
+
+;; (define (make-interval a b) (cons a b))
+
+;; Define selectors upper-bound and lower-bound to complete the implementation.
+
+;;Constructor
+(define (make-interval a b)
+  (if (< a b)
+      (cons a b)
+      (cons b a)))
+
+;;Selector
+;;lower boundary
+(define (lower-bound i)
+  (car i))
+
+;;upper boundary
+(define (upper-bound i)
+  (cdr i))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 2.8
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Using reasoning analogous to Alyssa's, describe how the difference of two intervals may be computed. Define a corresponding subtraction procedure, called sub-interval.
+
+;;Operator
+;;differ
+(define (int-differ i1 i2)
+  (make-interval (- (lower-bound i1) (upper-bound i2))
+                 (- (upper-bound i1) (lower-bound i2))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 2.9
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;The width of an interval is half of the difference between its upper and lower bounds. The width is a measure of the uncertainty of the number specified by the interval. For some arithmetic operations the width of the result of combining two intervals is a function only of the widths of the argument intervals, whereas for others the width of the combination is not a function of the widths of the argument intervals. Show that the width of the sum (or difference) of two intervals is a function only of the widths of the intervals being added (or subtracted). Give examples to show that this is not true for multiplication or division.
+
+;;Selector
+;;Width
+(define (int-width i)
+  (- (upper-bound i) (lower-bound i)))
+
+;;Operator
+;;Sum
+(define (int-sum i1 i2)
+  (make-interval (+ (lower-bound i1) (lower-bound i2))
+                 (+ (upper-bound i1) (upper-bound i2))))
+
+
+;;Display a formula
+(define (disformula a b c op)
+  (display a)
+  (display " ")
+  (display op)
+  (display " ")
+  (display b)
+  (display " = ")
+  (display c))
+
+;;A Left Right Arrow
+(define (lrarrow) (display " <=> "))
+
+;;to verify just test
+(define (test-for-width n max sign? sf df)
+  ;; Generate the negtive or the positive sign
+  (define (gen-negpos x)
+    (if (= 0 (random 2))
+        (- x)
+        x))
+
+  ;; Generate the number
+  (define (gen-num)
+    (if (= 0 sign?)
+        (gen-negpos (* (/ (random 1000) 1000.0) max))
+        (* (/ (random 1000) 1000.0) max)))
+
+  ;;A single test
+  (define (single-test)
+
+    ;;Test-result
+    (define (test-result sum op1 ws op ca st)
+      (newline)
+      (disformula i1 i2 sum op1)
+      (lrarrow)
+      (op w1 w2 ws)
+      (display "?")
+      (display ca)
+      (display " ")
+      (display st))
+
+    (define i1 (make-interval (gen-num) (gen-num)))
+    (define i2 (make-interval (gen-num) (gen-num)))
+    (define w1 (int-width i1))
+    (define w2 (int-width i2))
+    (define differ (int-differ i1 i2))
+    (define wd (int-width differ))
+    (define sum (int-sum i1 i2))
+    (define ws (int-width sum))
+    (define cs ((guess-op sf) w1 w2))
+    (define cd ((guess-op df) w1 w2))
+    (define st (= cs ws))
+    (define dt (= cd wd))
+    (test-result sum "+" ws (guess-pr sf) cs st)
+    (test-result differ "-" wd (guess-pr df)  cd dt)
+    (newline)
+    (and st dt))
+
+  (if (< n 1)
+      (display "Assumption Correct!\n")
+      (if (single-test)
+          (test-for-width (- n 1) max sign? sf df)
+          (display "Error, Assumption not correct!\n"))))
+
+;;guess for addion, is a cons, the first part tells how to operator a two-input function, the second tells how to print it.
+;;Constructor
+(define (make-guess x y)
+  (cons x y))
+
+;;Selector
+;;get the operator part
+(define (guess-op x)
+  (car x))
+;;get the print part
+(define (guess-pr x)
+  (cdr x))
+
+;;Instance
+(define (print-add x y d)
+  (disformula x y d "+"))
+
+(define (print-minus x y d)
+  (disformula x y d "-"))
+
+(define simple-add (make-guess +
+                               print-add))
+
+(define simple-minus (make-guess -
+                                 print-minus))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 2.10
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Ben Bitdiddle, an expert systems programmer, looks over Alyssa's shoulder and comments that it is not clear what it means to divide by an interval that spans zero. Modify Alyssa's code to check for this condition and to signal an error if it occurs.
+
+;;Operator
+;;multply interval
+(define (int-mul i1 i2)
+  (let ((p1 (* (lower-bound i1) (lower-bound i2)))
+        (p2 (* (lower-bound i1) (upper-bound i2)))
+        (p3 (* (upper-bound i1) (upper-bound i2)))
+        (p4 (* (upper-bound i1) (lower-bound i2))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+;;has 0, determind whether 0 is in the interval
+(define (has-0 i)
+  (or (= 0 (lower-bound i))
+      (= 0 (upper-bound i))
+      (and (> 0 (lower-bound i))
+           (< 0 (upper-bound i)))))
+
+;;divide interval
+(define (int-div i1 i2)
+  (if (has-0 i)
+      (display "Error: Interval has 0 inside, causing the interval to become two!\n")
+      (int-mul i1
+               (make-interval (/ 1.0 (upper-bound i2))
+                              (/ 1.0 (lower-bound i2))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 2.11
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
