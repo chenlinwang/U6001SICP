32b92f9320797e0886c5e6415e9a4b5595f07912
chenlinwang <chenlin.wang520@gmail.com>
2014-06-05 21:46:02 +0800

    Doing L6: Finish Exercise 1.3.4

--- BookExercise/sec1.3.scm
+++ BookExercise/sec1.3.scm
@@ -328,3 +328,140 @@
     (define (dtan i)
         (- (* 2 i) 1))
     ((make-cont-frac ntan dtan) k))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 1.40
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Define a procedure cubic that can be used together with the newtons-method procedure in expressions of the form:
+;; (newtons-method (cubic a b c) 1)
+;; to approximate zeros of the cubic x^{3} + ax_{2} + bx + c.
+(load "../BookImplementation/sec1.3.scm")
+
+(define (make-cube-fix a b c)
+    (lambda (x) (+ (cube x) (* a (square x)) (* b x) c (- x))))
+
+(define (solve-cube init tolerance a b c)
+    (find-fix2 (make-cube a b c) init tolerance))
+
+(define (solve-cube-newton init tolerance a b c)
+    (find-fix2 (newton (make-cube a b c)) init tolerance))
+
+;(display (solve-cube-newton 10 0.001 -30 20 5))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 1.41
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Define a procedure =double= that takes a procedure of one argument as argument and returns a procedure that applies the original procedure twice. For example, if =inc= is a procedure that adds 1 to its argument, then =(double inc)= should be a procedure that adds 2. What value is returned by
+;; (((double (double double)) inc) 5)
+;;This assume that inc-like procedure takes a single input
+(define (double p)
+    (lambda (x) (p (p x))))
+
+(define (inc x) (+ x 1))
+
+;(display (((double (double double)) inc) 5))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 1.42
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Let =f= and =g= be two one-argument functions. The composition =f= after =g= is defined to be the function =x = f(g(x))=. Define a procedure compose that implements composition. For example, if inc is a procedure that adds 1 to its argument.
+;; ((compose square inc) 6)
+(define (compose g f)
+    (lambda (x) (g (f x))))
+
+;(display ((compose square inc) 6))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 1.43
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; If =f= is a numerical function and =n= is a positive integer, then we can form the nth repeated application of =f=, which is defined to be the function whose value at =x= is =f(f(...(f(x))...))=. For example, if =f= is the function =f(x) = x + 1=, then the nth repeated application of f is the function =f(x) = x + n=. If =f= is the operation of squaring a number, then the nth repeated application of f is the function that raises its argument to the 2n th power. Write a procedure that takes as inputs a procedure that computes =f= and apositive integer n and returns the procedure that computes the nth repeated application of =f=. Your procedure should be able to be used as follows:
+;; ((repeated square 2) 5)
+;; : 625
+;; Hint: You may find it convenient to use compose from exercise 1.42.
+(define (repeat fun n)
+    (define (repeat-iter base i max)
+        (if (= i max)
+            base
+            (repeat-iter (compose fun base) (+ i 1) max)))
+  (repeat-iter fun 1 n))
+
+(define (repeat-fast fun n)
+    (define (identity x) x)
+  (define (fast-iter base sum i)
+      (cond ((= i 1) (compose base sum))
+            ((even? i) (fast-iter (double base) sum (/ i 2)))
+            (else (fast-iter base (compose base sum) (- i 1)))))
+  (fast-iter fun identity n))
+
+;(display ((repeat square 2) 5))
+;; (display ((repeat-fast square 3) 5))
+;; (newline)
+;; (display (* 625 625))
+;; (newline)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 1.44
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; The idea of smoothing a function is an important concept in signal processing. If =f= is a function and =dx= is some small number, then the smoothed version of =f= is the function whose value at a point x is the average of f(x - dx), f(x), and f(x + dx). Write a procedure smooth that takes as input a procedure that computes f and returns a procedure that computes the smoothed f. It is sometimes valuable to repeatedly smooth a function (that is, smooth the smoothed function, and so on) to obtained the n-fold smoothed function. Show how to generate the n-fold smoothed function of any given function using smooth and repeated from exercise 1.43.
+
+(define (smooth f dx)
+    (lambda (x) (/ (+ (f (- x dx)) (f x) (f (+ x dx))) 3.0)))
+
+(define (smooth-repeat f dx n)
+    (repeat (smooth f dx) n))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 1.45
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; We saw in section 1.3.3 that attempting to compute square roots by naively finding a fixed x/y does not converge, and that this can be fixed by average damping. The same method point of y works for finding cube roots as fixed points of the average-damped x/y^{2} . Unfortunately, the process does not work for fourth roots -- a single average damp is not enough to make a fixed-point search for x/y^{3} converge. On the other hand, if we average damp twice (i.e., use the average damp of the average damp of x/y_{3} ) the fixed-point search does converge. Do some experiments to determine how many average damps are required to compute nth roots as a fixed-point search based upon repeated average damping of x/y^{n-1} . Use this to implement a simple procedure for computing nth roots using fixed-point, average-damp, and the repeated procedure of exercise 1.43. Assume that any arithmetic operations you need are available as primitives.
+
+(define (fast-expr base n)
+    (define (fast-iter base sum i)
+        (cond ((= i 1) (* base sum))
+              ((even? i) (fast-iter (square base) sum (/ i 2)))
+              (else (fast-iter base (* sum base) (- i 1)))))
+  (fast-iter base 1 n))
+
+(define (make-nth-root x n)
+    (define n-1 (- n 1))
+    (lambda (y) (/ x (fast-expr y n-1))))
+
+(define (make-nth-damping fun n)
+    ((repeat-fast average-damping (- n 1)) fun))
+
+(define (find-nth-root x n init tolerance)
+    (find-fix2 (make-nth-damping (make-nth-root x n) n) init tolerance))
+
+;; (display (find-nth-root 16 4 1 0.01))
+;; (newline)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 1.46
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Several of the numerical methods described in this chapter are instances of an extremely general computational strategy known as iterative improvement. Iterative improvement says that, to compute something, we start with an initial guess for the answer, test if the guess is good enough, and otherwise improve the guess and continue the process using the improved guess as the new guess. Write a procedure iterative-improve that takes two procedures as arguments: a method for telling whether a guess is good enough and a method for improving a guess. Iterative-improve should return as its value a procedure that takes a guess as argument and keeps improving the guess until it is good enough. Rewrite the sqrt procedure of section 1.1.7 and the fixed-point procedure of section 1.3.3 in terms of iterative-improve.
+
+(define (make-iterative close? improve)
+    (define (iter init)
+      (let ((next (improve init)))
+        (if (close? init next)
+            init
+            (iter next))))
+  iter)
+
+;; tolerance maker for close?
+(define (tolerance-close? tolerance)
+    (lambda (a b) (close-enough? a b tolerance)))
+
+;; rewrite the find fixpoint
+(define (find-fix3 fun init tolerance)
+    ((make-iterative (tolerance-close? tolerance) fun) init))
+
+;; square root improve function
+(define (sqrt-improve y)
+    (lambda (x) (average x (/ y x))))
+
+;; rewrite square improvement
+(define (sqrt146 y init tolerance)
+    ((make-iterative (tolerance-close? tolerance) (sqrt-improve y)) init))
+
+;; test
+;;(display (sqrt146 2 1 0.01))
