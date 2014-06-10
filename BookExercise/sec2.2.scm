0a78960691dcbd2062efc92f1f17edca90e29c94
Chenlin Wang <chenlin.wang520@gmail.com>
2014-06-10 09:26:31 +0800

    Start L8, Finish Book 2.2.2

--- BookExercise/sec2.2.scm
+++ BookExercise/sec2.2.scm
@@ -76,9 +76,121 @@
                    (first-denomination coin-values))
                 coin-values)))))
 
-(define us-coins (list 50 25 10 5 1))
-(define us-coins-r (list-reverse us-coins))
-(display (cc 100 us-coins))
-(newline)
-(display (cc 100 us-coins-r))
-(newline)
+;; (define us-coins (list 50 25 10 5 1))
+;; (define us-coins-r (list-reverse us-coins))
+;; (display (cc 100 us-coins))
+;; (newline)
+;; (display (cc 100 us-coins-r))
+;; (newline)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 2.20
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; The procedures +, *, and =list= take arbitrary numbers of arguments. One way to define such procedures is to use =define= with =dotted-tail notation=. In a procedure definition, a parameter list that has a dot before the last parameter name indicates that, when the procedure is called, the initial parameters (if any) will have as values the initial arguments, as usual, but the final parameter's value will be a list of any remaining arguments. For instance, given the definition
+
+;; (define (f x y . z) <body>)
+
+;; the procedure =f= can be called with two or more arguments. If we evaluate
+
+;; (f 1 2 3 4 5 6)
+
+;; then in the body off, x will be 1, y will be 2, and z will be the list (3 4 5 6). Given the definition
+
+;; (define (g . w) <body>)
+
+;; the procedure g can be called with zero or more arguments. If we evaluate
+
+;; (g 1 2 3 4 5 6)
+
+;; then in the body of g,w will be the list (1 2 3 4 5 6).
+;; Use this notation to write a procedure =same-parity= that takes one or more integers and returns a list of all the arguments that have the same even-odd parity as the first argument. For example,
+
+;; (same-parity 1 2 3 4 5 6 7)
+;; (1 3 5 7)
+;; (same-parity 2 3 4 5 6 7)
+;; (2 4 6)
+
+(define (same-parity x . y)
+  ;; Return a list of numbers that have the same parity of the x. x: first input number;y:the rest of number
+  ;; (number . number) -> (list)
+  (define x-remainder (remainder x 2))
+  (define (test-parity z)
+    ;; Test the parity of z is the same to x. z: input number
+    ;; (number) -> (boolean)
+    (= (remainder z 2) x-remainder))
+  (do ((candidlist y (cdr candidlist))
+       (test-element (car y) (car candidlist))
+       (paritylist (list x) (if (test-parity test-element)
+                                (append paritylist (list test-element))
+                                paritylist)))
+      ((null? candidlist) paritylist)))
+
+;; (display (same-parity 1 3 5 4 6 8))
+;; (newline)
+;; (display (same-parity 2 4 3 5 6 9))
+;; (newline)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 2.21
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; The procedure =square-list= takes a list of numbers as argument and returns a list of the squares of those numbers.
+
+;; (square-list (list 1 2 3 4))
+;; (1 4 9 16)
+
+;; Here are two different definitions of =square-list=. Complete both of them by filling in the missing expressions:
+
+;; (define (square-list items)
+;;   (if (null? items)
+;;       nil
+;;       (cons <??> <??>)))
+;; (define (square-list items)
+;;   (map <??> <??>))
+
+(define (square x) (* x x))
+
+(define (square-list1 items)
+  ;; Return a list of number that is square of the input. items: the input list.
+  ;; (list) -> (list)
+  (if (null? items)
+      (list)
+      (cons (square (car items)) (square-list1 (cdr items)))))
+
+(define (square-list2 items)
+    ;; Return a list of number that is square of the input. items: the input list.
+  ;; (list) -> (list)
+  (map square items))
+
+;; (display (square-list1 (list 1 2 3 4)))
+;; (newline)
+;; (display (square-list2 (list 1 2 3 4)))
+;; (newline)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 2.23
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; The procedure =for-each= is similar to =map=. It takes as arguments a procedure and a list of elements. However, rather than forming a list of the results, =for-each= just applies the procedure to each of the elements in turn, from left to right. The values returned by applying the procedure to the elements are not used at all -- =for-each= is used with procedures that perform an action, such as printing. For example,
+
+;; (for-each (lambda (x) (newline) (display x))
+;;           (list 57 321 88))
+;; 57 321 88
+
+;; The value returned by the call to for-each (not illustrated above) can be something arbitrary, such as true. Give an implementation of =for-each=.
+
+(define (own-foreach func l)
+  ;; Self defined foreach.Apply func to  each element of l. func: the procedure;l: list of input element.
+  ;; ((A->B),list) -> nil
+  (do ((inputlist l (cdr inputlist))
+       (output #f (func (car inputlist))))
+      ((null? inputlist))))
+
+;; (own-foreach (lambda (x) (display x) (newline)) (list 1 2 3 4 5))
+
+(define (own-foreach2 func l)
+  ;; Self defined foreach.Apply func to  each element of l. func: the procedure;l: list of input element.
+  ;; ((A->B),list) -> nil
+  (do ((x l (cdr x))
+       (output #f (func (car x))))
+      ((null? x)))
+)
+
+(own-foreach2 (lambda (x) (display x) (newline)) (list 1 2 3))
